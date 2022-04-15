cb46c0dc09b7a789a1288033267af49c13f6559c
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-16 00:04:18 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layers
+from Config import config
+
+API_ID = config.API_ID
+API_HASH = config.API_HASH
+BOT_TOKEN = config.BOT_TOKEN
+
+class function(Client):
+        
+        
+        def __init__(self):
+                super().__init(
+                        api_id=API_ID,
+                        api_hash=API_HASH,
+                       bot_token=BOT_TOKEN,
+                       workers=50,
+                       plugins={"root": "resources"},
+                        )
+        async def start(self):
+               await super().start()
+               me = await self.get_me()
+               self.username = "@" + me.username
+               print(f"{me.frist_name} with for Pyrogram v{__version__} (Layer {layer}) Started on {me.username}")
+
+        async def stop(self, *args):
+               super().stop()
+               print("Bot stopped, Bye")
+
+bot = function()
+bot.run()
