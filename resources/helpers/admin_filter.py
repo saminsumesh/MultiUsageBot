b711d586e4e6e55a9752a152b2d1b5f2b48755b6
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-17 00:46:35 +0530

    Update and rename resources/kick.py to resources/helpers/admin_filter.py

---
+++ resources/helpers/admin_filter.py
@@ -0,0 +1,23 @@
+from pyrogram.types import *
+      
+
+async def admin_check(message: Message):
+	if not message.from_user:
+		return False
+      
+      if message.chat.type not in ["supergroup", "channel"]:
+      	return False
+
+      if message.from_user.id in [123456, 1468900900]:
+        return True
+
+      client = message._client 
+      user_id = message.from_user.id
+      chat_id = message.chat.id
+      
+      admin_status = await client.get_chat_members(chat_id, user_id)
+      check_admin = ["administrator", "creator"]
+      if admin_status not in check_admin:
+      	return False
+      else:
+      	return True
