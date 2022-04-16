9e598e9cfaaf26a215df5b49b33cb050d9aa14aa
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-17 00:11:59 +0530

    Update kick.py

--- resources/kick.py
+++ resources/kick.py
@@ -11,7 +11,7 @@ async def kick(client, message):
            user_client = await client.get_chat_members(chat_id, user_id)
            admin_check = ["creator", "administrator"]
            if user_client.status not in admin_check:
-               await message.reply_text("Sorry you're not an admin")
+               return await message.reply_text("Sorry you're not an admin")
            else:
                await client.ban_chat_member(chat_id, user_id,  int(time()) + 400)
                await message.reply_text("One has turned into bytes")
