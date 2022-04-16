2f9b3b7e988f57f0bd8ed50e150bae7c91b17674
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-17 01:00:15 +0530

    Create pin.py

---
+++ resources/modules/pin.py
@@ -0,0 +1,27 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from helpers.admin_filter import admin_check
+
+@Client.on_message(filters.command("pin") & admin_check)
+async def pin(_, message: Message):
+	if not message.reply_to_message:
+		await message.reply_text("Please reply to a message")
+		return
+	else:
+		await message.reply_to_message.pin()
+		await message.reply("Sucessfully Pinned The Message")
+		await asyncio.sleep(10)
+		await message.delete()
+		
+@Client.on_message(filters.command("unpin", "!") & admin_check)
+async def unpin(_, message: Message):
+	if not message.reply_to_message:
+		await message.reply_text("Please reply to a message")
+		return
+	else:
+		await message.reply_to_message.unpin()
+		await message.reply_text("Successfully unpinned the message")
+		await asyncio.sleep(10)
+		await message.delete()
+	
