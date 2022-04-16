9ed3e5c1ff047fec3a2c71ebdce96e01ad20b134
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-16 22:55:15 +0530

    Update kick.py

--- resources/kick.py
+++ resources/kick.py
@@ -7,7 +7,7 @@ async def kick(client: Client, message):
       chat_id = message.chat.id
       reply_m = message.reply_to_message
       if reply_m:
-            user = await client.get_chat_memeber(message.chat.id, reply_m.from_user.id)
+            user = await client.get_chat_memebers(message.chat.id, reply_m.from_user.id)
             if user.status not in ("administrator" or "creator"):
                   await message.reply_text("Sorry you're not an admin")
             else:
