e4f1fba9c6573846453f35c5ab5e81088d644056
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-16 22:33:45 +0530

    Update start.py

--- resources/start.py
+++ resources/start.py
@@ -4,8 +4,7 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("start") & filters.private)
 async def cmd_start(client: Client, message):
-      pp=await message.reply_sticker(
-            sticker = "CAACAgUAAxkBAAECSC5iWtY31mtvT4ykobd0N4tw9ZmR_wACRwIAAlJa9jtj_y8Dm5CUrB4E")    
-      await asyncio.sleep(5)    
-      await pp.edit("Hi")  
-            
+      username = message.from_user.first_name
+      await message.reply_text(f"Hi {username}")
+            )    
+                   
