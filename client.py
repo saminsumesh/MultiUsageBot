b7bdc58b697fbe018147a394cee41d8acb201b7f
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-16 20:53:28 +0530

    Update client.py

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ class Function(Client):
         
         def __init__(self):
                 super().__init__(
+                        "OfferCloseSoon",
                         api_id=API_ID,
                         api_hash=API_HASH,
                         bot_token=BOT_TOKEN,
