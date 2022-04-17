9b57adb6b75b09302abce107ac47cd2974d800f8
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-18 00:05:01 +0530

    Create dbtoggle.py

---
+++ resources/helpers/dbtoggle.py
@@ -0,0 +1,28 @@
+
+
+log.info("Initializing MongoDB client")
+mongo_client = MongoClient(MONGO_URL)
+db = mongo_client.offbot
+
+antiservicedb = db.antiservice
+
+
+async def is_antiservice_on(chat_id: int) -> bool:
+    chat = await antiservicedb.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
+
+
+async def antiservice_on(chat_id: int):
+    is_antiservice = await is_antiservice_on(chat_id)
+    if is_antiservice:
+        return
+    return await antiservicedb.delete_one({"chat_id": chat_id})
+
+
+async def antiservice_off(chat_id: int):
+    is_antiservice = await is_antiservice_on(chat_id)
+    if not is_antiservice:
+        return
+    return await antiservicedb.insert_one({"chat_id": chat_id})
