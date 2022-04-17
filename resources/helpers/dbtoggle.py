7cba9c773cfdb76aaccffd0c3fd25bebb8bc460b
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-18 00:17:49 +0530

    Update dbtoggle.py

--- resources/helpers/dbtoggle.py
+++ resources/helpers/dbtoggle.py
@@ -1,4 +1,24 @@
+import time
 
+class Log:
+    def __init__(self, save_to_file=False, file_name="wbb.log"):
+        self.save_to_file = save_to_file
+        self.file_name = file_name
+
+    def info(self, msg):
+        print(f"[+]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[INFO]({time.ctime(time.time())}): {msg}\n")
+
+    def error(self, msg):
+        print(f"[-]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[ERROR]({time.ctime(time.time())}): {msg}\n")
+
+
+log = Log(True, "bot.log")
 
 log.info("Initializing MongoDB client")
 mongo_client = MongoClient(MONGO_URL)
