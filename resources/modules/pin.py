1f47246d8de7a2aae82c2b62f8d22aa03bfc24fa
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-17 11:31:28 +0530

    Update pin.py

--- resources/modules/pin.py
+++ resources/modules/pin.py
@@ -3,19 +3,16 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from resources.helpers.admin_filter_m import admin_misc
 
-@Client.on_message(filters.command("pin", "!") & admin_misc)
+@Client.on_message(filters.command("pin") & admin_misc)
 async def pin(_, message: Message):
 	if not message.reply_to_message:
-            return
-        await message.reply_to_message.pin()
-        await message.reply("Sucessfully Pinned The Message")
+		return
+	await message.reply_to_message.pin()
+	await message.reply_text("Message have been pinned")
 	
-@Client.on_message(filters.command("unpin", "!") & admin_misc)
+@Client.on_message(filters.command("unpin") & admin_misc)
 async def unpin(_, message: Message):
 	if not message.reply_to_message:
-            return
+		return
 	await message.reply_to_message.unpin()
-	await message.reply_text("Successfully unpinned the message")
-	await asyncio.sleep(10)
-	await message.delete()
-	
+	await message.reply_text("Message have been unpinned")
