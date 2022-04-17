05d79d7e1d8d69050e569105cd8f7b0c8847e9d4
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-17 11:16:49 +0530

    Update pin.py

--- resources/modules/pin.py
+++ resources/modules/pin.py
@@ -6,22 +6,18 @@ from resources.helpers.admin_filter_m import admin_misc
 @Client.on_message(filters.command("pin", "!") & admin_misc)
 async def pin(_, message: Message):
 	if not message.reply_to_message:
-		await message.reply_text("Please reply to a message")
 		return
-	else:
-		await message.reply_to_message.pin()
-		await message.reply("Sucessfully Pinned The Message")
-		await asyncio.sleep(10)
-		await message.delete()
+        await message.reply_to_message.pin()
+        await message.reply("Sucessfully Pinned The Message")
+	await asyncio.sleep(10)
+	await message.delete()
 		
 @Client.on_message(filters.command("unpin", "!") & admin_misc)
 async def unpin(_, message: Message):
 	if not message.reply_to_message:
-		await message.reply_text("Please reply to a message")
 		return
-	else:
-		await message.reply_to_message.unpin()
-		await message.reply_text("Successfully unpinned the message")
-		await asyncio.sleep(10)
-		await message.delete()
+	await message.reply_to_message.unpin()
+	await message.reply_text("Successfully unpinned the message")
+	await asyncio.sleep(10)
+	await message.delete()
 	
