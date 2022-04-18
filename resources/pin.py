1e52bfddeb368c888a6799ee705dac89365d23ea
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-18 12:57:00 +0530

    Create pin.py

---
+++ resources/pin.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+
+
+client = client
+
+@client.on_message(filters.command("pin") & filters.group)
+async def pin(client, message):
+	chat_id = message.chat.id
+	
+	if not message.reply_to_message:
+		return await message.reply_text("Please reply to a message")
+	else:
+		admin_check = await client.get_chat_members(chat_id, message.from_user.id)
+		if amdin_check.status not in "administrator" or "creator":
+			await message.reply_text("You're not an admin of this group")
+		else:
+			await client.pin_chat_message(chat_id, message_id)
+			return True
