0b586b64a0f6bd682b1bf8ad18d51257bc042bc6
Samin Sumesh <91833744+saminsumesh@users.noreply.github.com>
2022-04-18 13:46:07 +0530

    Update pin.py

--- resources/pin.py
+++ resources/pin.py
@@ -11,7 +11,7 @@ async def pin(client, message):
 		return await message.reply_text("Please reply to a message")
 	else:
 		admin_check = await client.get_chat_members(chat_id, message.from_user.id)
-		if admin_check.status not in "administrator" or "creator":
+		if not admin_check.status == "administrator" or admin_check.status == "creator":
 			await message.reply_text("You're not an admin of this group")
 		else:
 			await client.pin_chat_message(chat_id, message.reply_to_message.id)
